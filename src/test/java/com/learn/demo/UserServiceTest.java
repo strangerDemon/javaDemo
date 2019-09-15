9417b39cff9038ce41d7b4fa376eabce43d54a57
strangerDemon <735359571@qq.com>
2019-09-15 20:08:51 +0800

    单元测试

---
+++ src/test/java/com/learn/demo/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.learn.demo;
+
+import com.learn.demo.Entity.UserEntity;
+import com.learn.demo.Service.UserService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 单元测试
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+    @Resource
+    private UserService userService;
+
+    @Test
+    public void getByUserId(){
+        List<UserEntity> list=userService.findAll();
+        Assert.assertEquals("123456",list.get(0).getPassword());
+    }
+}
