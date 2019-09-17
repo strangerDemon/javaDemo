68c54eb531890f8e66817144350521cb3477ee58
strangerDemon <735359571@qq.com>
2019-09-17 14:26:48 +0800

    custom read yml

--- src/main/java/com/learn/demo/Model/SystemConfigModel.java
+++ src/main/java/com/learn/demo/Model/SystemConfigModel.java
@@ -1,5 +1,8 @@
 package com.learn.demo.Model;
 
+
+import com.learn.demo.Utils.YmlFactoryUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -10,13 +13,13 @@ import org.springframework.stereotype.Component;
  * @description 系统配置参数
  */
 @Component
-//@PropertySource(value = {"classpath:resources/systemConfig.yml"}) //引入配置文件路径
+@PropertySource(value = {"classpath:system.yml"},factory = YmlFactoryUtils.class,encoding = "utf-8") //引入配置文件路径,只支持.properties
+//@PropertySource(value = {"classpath:system.properties"},encoding = "utf-8")
 @ConfigurationProperties(prefix = "system") //和配置文件绑定，类中属性和配置文件必须一致才能注入
 public class SystemConfigModel {
     //@Value配合 @PropertySource
-    //@value("${system.redisTimeOut")
+    //@Value("${system.redisTimeOut}")
     private Integer redisTimeOut;
-
 
     public Integer getRedisTimeOut() {
         return redisTimeOut;
