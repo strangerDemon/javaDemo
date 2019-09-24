b2af44cdbf0be9ee87e2d9c4353b04031463e3e3
strangerDemon <735359571@qq.com>
2019-09-24 18:02:10 +0800

    cors filter

---
+++ src/main/java/com/learn/demo/aop/MyCorsFilter.java
@@ -0,0 +1,44 @@
+package com.learn.demo.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨越过滤处理.
+ *
+ * @author demo
+ * @version 1.0.0
+ * @date 2019/9/24 16:21
+ */
+@Slf4j
+@Component
+@Configuration
+public class MyCorsFilter {
+
+  /**
+   * .
+   *
+   * @return FilterRegistrationBean
+   */
+  @Bean
+  public FilterRegistrationBean corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("http://localhost:8889");
+    config.addAllowedOrigin("null");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(0);
+    return bean;
+  }
+
+}
